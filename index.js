bfc1c9fef0acfb0c24629cc9e910bfd04644d04b
ilhambou <bouichilham2@gmail.com>
2022-08-14 03:09:23 +0200

    c1(port mongodb)

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+// app.get("/test", (req,res)=>{
+//     res.send("it works");
+// })
+
+mongoose.connect(process.env.MDB_CONNECT,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, 
+(err) => {
+        if(err) return console.error(err);
+        console.log("connected to mongodb");
+    });
