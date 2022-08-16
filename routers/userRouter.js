a2aa69ae173524c4afee965c23b6438edd0a5d76
ilhambou <bouichilham2@gmail.com>
2022-08-16 04:25:51 +0200

    c3(login logout)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,6 +3,7 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//Register
 
 router.post("/", async (req, res) => {
     try {
@@ -71,5 +72,68 @@ router.post("/", async (req, res) => {
         res.status(500).send();
       }
     });
+
+// log in
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validate
+
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ errorMessage: "Please enter all required fields." });
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(401).json({ errorMessage: "Wrong email or password." });
+
+    const passwordCorrect = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+    if (!passwordCorrect)
+      return res.status(401).json({ errorMessage: "Wrong email or password." });
+
+    // sign the token
+
+    const token = jwt.sign(
+      {
+        user: existingUser._id,
+      },
+      ""+process.env.JWT_SECRET
+    );
+
+    // send the token in a HTTP-only cookie
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
+      .send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      secure: true,
+      sameSite: "none",
+    })
+    .send();
+});
+
+
+
+    
 
 module.exports = router;
